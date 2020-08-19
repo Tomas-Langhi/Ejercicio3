f4d968dd92b2536c57ff18d5d89d9fb8b7048677
Tomas Langhi <tomas-langhi>
2020-08-19 19:12:27 -0300

    Se crearon los modelos y se los registro en el admin

--- Biblio/admin.py
+++ Biblio/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from Biblio.models import *
 
 # Register your models here.
+admin.site.register(Libro,)
+admin.site.register(Revista,)
+admin.site.register(Alumno,)
+admin.site.register(Profesor,)
+admin.site.register(Prestamo,)
--- Biblio/models.py
+++ Biblio/models.py
@@ -1,6 +1,46 @@
 from django.db import models
 
 # Create your models here.
-class Libro(models.Model):
-    editorial = models.CharField(max_length=50, default="");
-    
+class Material(models.Model):
+    codigo = models.CharField(max_length=50, default="")
+    autor = models.CharField(max_length=50, default="")
+    titulo = models.CharField(max_length=50, default="")
+    anio = models.IntegerField(null = True)
+    status = models.CharField(max_length=50, default="")
+
+    def __str__(self):
+        return str(self.titulo)
+
+class Libro(Material):
+    editorial = models.CharField(max_length=50, default="")
+
+class Revista(Material):
+    numero = models.IntegerField(null = True)
+
+class Persona(models.Model):
+    nombre = models.CharField(max_length=50, default="")
+    apellido = models.CharField(max_length=50, default="")
+    correo = models.CharField(max_length=50, default="")
+    telefono = models.IntegerField(null = True)
+    numLibros = models.IntegerField(null=True)
+    adeudo = models.FloatField(null = True)
+
+    def __str__(self):
+        return str(self.nombre + " " + self.apellido)
+
+class Alumno(Persona):
+    matricula = models.IntegerField(null = True)
+
+class Profesor(Persona):
+    numEmpleado = models.IntegerField(null = True)
+
+class Prestamo(models.Model):
+    persona = models.ForeignKey( 'Persona', on_delete=models.CASCADE)
+    material = models.ForeignKey('Material', on_delete=models.CASCADE)
+    fechaSalida = models.DateField(auto_now=True)
+    fechaRegreso = models.DateField()
+
+    def __str__(self):
+        return str(self.persona.nombre + "--" + self.material.titulo)
+
+

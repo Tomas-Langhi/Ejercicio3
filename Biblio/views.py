54289a35204ec066876de9c49365499e16159c87
Tomas Langhi <tomas-langhi>
2020-08-27 12:23:55 -0300

    se modificaron los templates del admin y del home

--- Biblio/views.py
+++ Biblio/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from Biblio.models import *
 
 # Create your views here.
 
 class HomeView(TemplateView):
     template_name = 'Biblio/home.html'
+    
